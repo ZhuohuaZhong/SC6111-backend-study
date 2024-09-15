2e93aa9ad6ae1e8508f8c132bf16916fbe96a484
ZhuohuaZhong <zhong_zhuohua@outlook.com>
2024-09-15 22:02:59 +0800

    update Login layout

---
+++ frontend.js
@@ -0,0 +1,34 @@
+$(document).ready(function () {
+  $("#loginButton").click(function () {
+    var name = $("#loginNameInput").val();
+    var password = $("#loginPasswordInput").val();
+
+    console.log("Name: " + name);
+    console.log("Password: " + password);
+
+    $.ajax({
+      url: "http://127.0.0.1:8080/login",
+      contentType: "application/json",
+      type: "POST",
+      data: JSON.stringify({
+        name: name,
+        password: password,
+      }),
+      success: function (response) {
+        alert("Response: " + response.status);
+        console.log("Response: ", response.status);
+      },
+      error: function (xhr, status, error) {
+        alert("Error:", error);
+        console.error("Error: ", error);
+      },
+    });
+  });
+
+  $("#signUpChangeLayout").click(function () {
+    $("#title").text("Sign");
+  });
+  //   $("#signUpChangeLayout").click(function (){
+
+  //   });
+});
